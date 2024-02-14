78614cb2bbecd7e30cd59bbcea002684ffad4f07
minura pasan <minurapasan22@gmail.com>
2024-02-14 15:52:55 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Minura Pasan</h3>
+          <h3>Designed and Developed by Minura Pasan.</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>IOT Developing  {year} MS</h3>
